dd509397ceaa61861a98f43c2a35c95b7ab0e354
mmelle <aojmathiasmel@gmail.com>
2015-03-06 09:17:13 +0100

    0.1

---
+++ src/TowerOfHanoi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Skriv inn antall disker: ");
+        int n = input.nextInt();
+
+        System.out.println("Bevegelser gjort er: ");
+        moveDisks(n, 'A', 'B', 'C');
+    }
+
+    public static void moveDisks(int n, char fromTower, char toTower, char auxTower){
+        if (n == 1){
+            System.out.println("Flytter disk " + n + " fra " + fromTower + " til " + toTower);
+        } else {
+            moveDisks(n-1, fromTower, toTower, auxTower);
+            System.out.println("Flytter disk " + n + " fra " + fromTower + " til " + toTower);
+            moveDisks(n-1, auxTower, toTower, fromTower);
+        }
+    }
+}
