88fc50174b7fcf2e9588beca14d459bee278498a
mmelle <aojmathiasmel@gmail.com>
2015-03-06 09:22:33 +0100

    0.2

--- src/TowerOfHanoi.java
+++ src/TowerOfHanoi.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class TowerOfHanoi {
+    private static int movementCounter;
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
@@ -9,15 +10,18 @@ public class TowerOfHanoi {
 
         System.out.println("Bevegelser gjort er: ");
         moveDisks(n, 'A', 'B', 'C');
+        System.out.println("Antall bevegelser: " + movementCounter);
     }
 
     public static void moveDisks(int n, char fromTower, char toTower, char auxTower){
         if (n == 1){
             System.out.println("Flytter disk " + n + " fra " + fromTower + " til " + toTower);
+
         } else {
             moveDisks(n-1, fromTower, toTower, auxTower);
             System.out.println("Flytter disk " + n + " fra " + fromTower + " til " + toTower);
             moveDisks(n-1, auxTower, toTower, fromTower);
         }
+        movementCounter++;
     }
 }
